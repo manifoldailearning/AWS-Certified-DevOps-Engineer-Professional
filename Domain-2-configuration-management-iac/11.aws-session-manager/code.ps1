0a9e25d764fe77d3eb272d4f23f15bcfe3a4a1bf
nachiketh <support@manifoldailearning.in>
2023-08-30 23:17:02 +0530

    d2

--- Domain-2-configuration-management-iac/11.aws-session-manager/code.ps1
+++ Domain-2-configuration-management-iac/11.aws-session-manager/code.ps1
@@ -1,14 +1,10 @@
-$code = "activation-code"
+ $code = "activation-code"
 $id = "activation-id"
-$region = "region"
+$region = "us-east-1"
 $dir = $env:TEMP + "\ssm"
 New-Item -ItemType directory -Path $dir -Force
 cd $dir
-(New-Object 
-System.Net.WebClient).DownloadFile("https://amazonssm-$region.s3.amazonaws.co
-m/latest/windows_amd64/AmazonSSMAgentSetup.exe",           $dir             + 
-"\AmazonSSMAgentSetup.exe") 
-Start-Process   .\AmazonSSMAgentSetup.exe   -ArgumentList  @("/q",    "/log", 
-"install.log", "CODE=$code", "ID=$id", "REGION=$region") -Wait
+(New-Object System.Net.WebClient).DownloadFile("https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/windows_amd64/AmazonSSMAgentSetup.exe",$dir + "\AmazonSSMAgentSetup.exe") 
+Start-Process   .\AmazonSSMAgentSetup.exe   -ArgumentList  @("/q",    "/log","install.log", "CODE=$code", "ID=$id", "REGION=$region") -Wait
 Get-Content ($env:ProgramData + "\Amazon\SSM\InstanceData\registration")
-Get-Service -Name "AmazonSSMAgent"
+Get-Service -Name "AmazonSSMAgent" 
